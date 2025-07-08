21a19c07532c4f391a6a53cbe316c44ff53e89f7
João Pedro <contas.joaopedro.favoretti@gmail.com>
2025-07-08 16:59:14 -0300

    Fixing the layout

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cesium from "vite-plugin-cesium";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cesium(), tailwindcss()],
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), cesium()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
