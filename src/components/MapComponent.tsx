e59c28a843e20d7d1e058dbe543dd350996527c7
João Pedro <contas.joaopedro.favoretti@gmail.com>
2025-07-15 11:30:35 -0300

    Adding Open Street Maps base layer in the world

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,8 +1,7 @@
 import { useRef, useEffect, Component, createRef } from "react";
 import { Matrix4, Cartesian3, Terrain, Ion, IonResource } from "cesium";
 import * as Cesium from "cesium";
-import { Viewer, useCesium, Cesium3DTileset } from "resium";
-import { CylinderTool } from "@/cylinder-tool.ts";
+import { Viewer, useCesium, Cesium3DTileset, ImageryLayer } from "resium";
 
 const IonKey = import.meta.env.VITE_ION_KEY;
 
@@ -16,17 +15,47 @@ class ViewerController {
       return;
     }
 
+    console.log("Trying to obtain the user current location");
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const { latitude, longitude } = position.coords;
+        console.log("User's current location:", latitude, longitude);
+        // viewer.camera.setView({
+        //   destination: Cartesian3.fromDegrees(longitude, latitude, 2000),
+        //   orientation: {
+        //     heading: Cesium.Math.toRadians(0),
+        //     pitch: Cesium.Math.toRadians(-90),
+        //     roll: 0,
+        //   },
+        // });
+      },
+    );
+
     viewer.cesiumWidget.creditContainer.remove();
 
-    viewer.camera.lookAt(
-      Cartesian3.fromDegrees(-45.873938, -23.212619, 200.0),
-      new Cartesian3(800.0, 800.0, 800.0),
-    );
+    // Default ICEA
+    viewer.camera.setView({
+      destination: Cartesian3.fromDegrees(-45.873938, -23.212619, 2000),
+      orientation: {
+        heading: Cesium.Math.toRadians(0),
+        pitch: Cesium.Math.toRadians(-90),
+        roll: 0,
+      },
+    });
 
     viewer.camera.lookAtTransform(Matrix4.IDENTITY);
 
-    new CylinderTool(viewer);
+    viewer.camera.moveEnd.addEventListener(() => {
+      const cameraPosition = viewer.camera.computeViewRectangle();
+      console.log("Camera position:", cameraPosition);
+    });
   }
+
+  private fetchDataForCurrentView = () => {
+    // Logic to determine bounding box and make API call
+    // (We'll cover this in the next section)
+    console.log("Fetching data for the new view...");
+  };
 }
 
 const ViewerManager = () => {
@@ -55,9 +84,19 @@ export default class MapComponent extends Component {
           fullscreenButton={false}
           navigationHelpButton={false}
           vrButton={false}
+          sceneModePicker={false}
+          baseLayerPicker={false}
           style={{ width: "100%", height: "100%" }}
         >
           <ViewerManager />
+          <ImageryLayer
+            imageryProvider={
+              new Cesium.OpenStreetMapImageryProvider({
+                url: "https://a.tile.openstreetmap.org",
+              })
+            }
+            show={true}
+          />
           <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
         </Viewer>
       </div>
