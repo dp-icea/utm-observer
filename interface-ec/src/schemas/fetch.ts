48d6efc4f38050c9d3cd1dc1780ab5f7846157fe
joaofavoretti <contas.joaopedro.favoretti@gmail.com>
2025-07-25 07:07:29 -0300

    Cleaning interface-ec

--- interface-ec/src/schemas/fetch.ts
+++
@@ -1,8 +0,0 @@
-import type { Constraint } from "./constraint";
-import type { OperationalIntent } from "./operational-intent";
-
-
-export interface QueryVolumesResponse {
-  operational_intents: OperationalIntent[];
-  constraints: Constraint[],
-}
