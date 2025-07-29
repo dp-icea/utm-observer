48512827fd812e6874ef05bc2b8ce60751cd54a6
João Pedro <contas.joaopedro.favoretti@gmail.com>
2025-07-29 10:22:36 -0300

    Fixing flight request error

--- backend/services/flights.py
+++ backend/services/flights.py
@@ -1,30 +1,34 @@
 from datetime import time
 from uuid import UUID
-from schemas.common.enums import Audition, Authority
+from schemas.common.base import Time
+from schemas.common.enums import Audition, Authority, TimeFormat
 from config.config import Settings
 from httpx import AsyncClient
 from services.dss.remoteid import DSSRemoteIDService
 from services.uss.remoteid import USSRemoteIDService
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from pprint import pprint
 
 from schemas.flights import (
-        QueryFlightsRequest,
-        QueryFlightsResponse,
+    QueryFlightsRequest,
+    QueryFlightsResponse,
 )
 
 RESOURCES_PATH = "/dasa-dp/api/telemetry"
 
+
 class FlightsService:
     def __init__(self):
         settings = Settings()
-            
+
         base_url = settings.BRUTM_BASE_URL
 
         if not base_url:
-            raise ValueError("BRUTM_BASE_URL must be set in the environment variables.")
+            raise ValueError(
+                "BRUTM_BASE_URL must be set in the environment variables.")
 
         self.client = AsyncClient(
-                base_url=base_url,
+            base_url=base_url,
         )
 
     async def query_flights(
@@ -35,7 +39,8 @@ class FlightsService:
         apikey = settings.BRUTM_KEY
 
         if not apikey:
-            raise ValueError("BRUTM_KEY must be set in the environment variables.")
+            raise ValueError(
+                "BRUTM_KEY must be set in the environment variables.")
 
         query_params = {
             "apikey": apikey,
@@ -53,20 +58,23 @@ class FlightsService:
 
         dssClient = DSSRemoteIDService()
 
-        area = query_params["lat1"] + "," + query_params["lng1"] + "," + \
-               query_params["lat2"] + "," + query_params["lng2"] + "," + \
-               query_params["lat3"] + "," + query_params["lng3"] + "," + \
-               query_params["lat4"] + "," + query_params["lng4"]
+        area = str(query_params["lat1"]) + "," + str(query_params["lng1"]) + "," + \
+            str(query_params["lat2"]) + "," + str(query_params["lng2"]) + "," + \
+            str(query_params["lat3"]) + "," + str(query_params["lng3"]) + "," + \
+            str(query_params["lat4"]) + "," + str(query_params["lng4"])
 
-        now = datetime.now(datetime.timezone.utc)
-        earliest_time = now.isoformat() + 'Z'
-        latest_time = (now + timedelta(seconds=10)).isoformat() + 'Z'
+        now = datetime.now(timezone.utc)
+        earliest_time = now.isoformat().replace("+00:00", "") + 'Z'
+        latest_time = (now + timedelta(seconds=10)
+                       ).isoformat().replace("+00:00", "") + 'Z'
 
-        isas = dssClient.search_identification_service_areas(
+        isas = await dssClient.search_identification_service_areas(
             area=area,
             earliest_time=earliest_time,
             latest_time=latest_time,
         )
+
+        pprint(isas)
 
         # Get Flights by ISA
         flights = []
@@ -79,8 +87,8 @@ class FlightsService:
                 )
                 flight_response = await ussClient.search_flights(
                     view=query_params["lat1"] + "," + query_params["lng1"] + "," +
-                        query_params["lat3"] + "," + query_params["lng3"],
-                        recent_positions_duration=0
+                    query_params["lat3"] + "," + query_params["lng3"],
+                    recent_positions_duration=0
                 )
 
                 flights.append(flight_response.flights)
@@ -89,13 +97,17 @@ class FlightsService:
                     "error": str(e),
                     "service_area": isa.uss_base_url
                 })
-        
 
-        response = QueryFlightsRequest(
+        pprint(flights)
+
+        response = QueryFlightsResponse(
             flights=flights,
             partial=False,
             errors=[],
-            timestamp=now,
+            timestamp=Time(
+                value=datetime.now(timezone.utc),
+                format=TimeFormat.RFC3339,
+            ),
         )
 
-        return QueryFlightsResponse.model_validate(response.json())
+        return response
